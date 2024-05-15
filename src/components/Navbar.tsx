5964c9396c7353c7d9419250ae3d4599157ee334
saidulhasan <saidulhasan562@gmail.com>
2024-05-15 18:54:51 +0600

    add authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,14 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL
+
   return (
     <nav className=" sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -68,7 +72,7 @@ const Navbar = () => {
                 >
                   Login
                 </Link>
-                <div className=" h-8 w-px bg-zinc-200 sm:block"/>
+                <div className=" h-8 w-px bg-zinc-200 sm:block" />
                 <Link
                   href="/configure/upload"
                   className={buttonVariants({
